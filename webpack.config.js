580e825abcf4d5e0ad012688973eb395c92eabee
march-dave <ysangy@gmail.com>
2019-09-14 14:26:48 -0400

    update webpakc options

--- webpack.config.js
+++ webpack.config.js
@@ -84,8 +84,6 @@ module.exports = {
               modules: true,
               importLoaders: 1,
               localIdentName: "[name]_[local]_[hash:base64]",
-              sourceMap: true,
-            //   minimize: true
             }
           }
         ]
